8da0ede26eda34f12dae8713de7dc32047e95180
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-05-09 14:24:19 -0400

    Object Basis2 and 3 complete

--- objects/temp.js
+++ objects/temp.js
@@ -1,4 +1,31 @@
-let bandInfo;
+/* const cat = {
+    name : 'Bertie',
+    breed : 'Cymric',
+    color : 'white',
+    greeting: function() {
+      console.log('Meow!');
+    }
+  }
+  
+  // Put your code here
+  
+  const catName = cat['name'];
+  cat.greeting();
+  cat['color'] = 'black';
+  
+  // Don't edit the code below here
+  
+  let para1 = document.createElement('p');
+  let para2 = document.createElement('p');
+  
+  para1.textContent = `The cat's name is ${ catName }.`;
+  para2.textContent = `The cat's color is ${ cat.color }.`;
+  
+  section.appendChild(para1);
+  section.appendChild(para2);
+   */
+
+/* let bandInfo;
 
 // Put your code here
 const greta = {
@@ -33,8 +60,32 @@ bandInfo = `${greta.name} is an ${greta.nationality} ${greta.genre} band formed 
 let para1 = document.createElement('p');
 para1.textContent = bandInfo;
 section.appendChild(para1);
-/* vocal: 'Josh Kiszka',
-guitar1: 'Jake Kiszka',
-bass:'Sam Kiszka',
-drums: 'Danny Wagner',
-} */
+
+// Band members
+// vocal: 'Josh Kiszka',
+// guitar1: 'Jake Kiszka',
+// bass:'Sam Kiszka',
+// drums: 'Danny Wagner',
+// } 
+*/
+
+/* const cat = {
+    name: 'Bertie',
+    breed: 'Cymric',
+    color: 'white',
+    greeting: function () {
+        console.log(`Hello, said ${this.name} the ${this.breed}`);
+    }
+}
+const cat2 = {
+    name: 'Mickey',
+    breed: 'Mix',
+    color: 'Black',
+    greeting: function () {
+        console.log(`Hello, said ${this.name} the ${this.breed}`);
+    }
+}
+ */
+
+
+
